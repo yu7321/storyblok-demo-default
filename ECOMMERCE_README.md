41951b12e465df5de12e4fa3dd115442e65c3a84
Roberto Butti <roberto.butti@gmail.com>
2023-03-03 21:52:23 +0100

    basic components for ecommerce

---
+++ ECOMMERCE_README.md
@@ -0,0 +1,5 @@
+## Ecommerce integration
+
+### components
+- [ ] New Arrivals component (for home page, showing image and product link)
+- [ ] Latest additions list of products (number is defined in the component), retrieving latest products (for sorting try to use this syntax: https://developers.swell.is/backend-api/querying/sorting) and for limiting this one: https://developers.swell.is/backend-api/querying/sorting#limiting
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
+
   modules: [
+
     [
       '@storyblok/nuxt',
       {
@@ -7,17 +9,28 @@ export default defineNuxtConfig({
         If you would like to use this as a template for your project, simply provide the access token here.
         Also please remove line 14 (usePlugin) as well as the file plugins/storyblok.js.
         */
-        // accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
-        usePlugin: false,
+        accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
+
+        //usePlugin: false,
       },
     ],
     '@nuxtjs/tailwindcss',
+
   ],
   ssr: false,
   css: ['@/assets/css/fonts.css'],
+
   runtimeConfig: {
     public: {
       templateToken: process.env.STORYBLOK_PREVIEW_TOKEN,
+      swellStoreName: process.env.SWELL_STORE_NAME,
+      swellAccessToken: process.env.SWELL_ACCESS_TOKEN
+
     },
+  },
+  publicRuntimeConfig: {
+      templateToken: process.env.STORYBLOK_PREVIEW_TOKEN,
+      swellStoreName: process.env.SWELL_STORE_NAME,
+      swellAccessToken: process.env.SWELL_ACCESS_TOKEN
   },
 })
