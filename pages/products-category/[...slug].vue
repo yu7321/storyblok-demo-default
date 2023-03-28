5f22e3aa130ae903b625cb6c193df85cfd310d77
Roberto Butti <roberto.butti@gmail.com>
2023-03-28 19:39:25 +0200

    Update Products Category page

--- pages/products-category/[...slug].vue
+++ pages/products-category/[...slug].vue
@@ -2,39 +2,13 @@
 import swell from 'swell-js'
 const route = useRoute()
 
-/**
- * Create correct slug and handle language parameter
- */
-
-// Here we are getting the path as a URL parameter
-let slug = []
-console.log(route.path)
-if (route.query.path) {
-    slug = route.query.path?.split('/')
-} else {
-    // fallback if no path parameter found (e.g. in template space)
-    slug = route.params.slug.slice()
-}
-// In your project you would typically want to do the following:
-// let slug = route.params.slug.slice()
-let language = 'default'
-
-if (slug) {
-    language = await getLanguage(slug)
-    // remove first slug entry if it matches query language
-    if (language !== 'default') slug = slug.slice(1)
-    slug = slug.join('/')
-} else {
-    slug = 'style-1'
-}
-if (slug === "product-category") {
+let pathArray = await getPathArray()
+let language = await getLanguage(pathArray)
+let slug = await getSlug(pathArray, 'style-1')
+if (slug === "default") {
     slug = 'style-1'
 }
 
-console.log(slug)
-/**
- * Fetch current release
- */
 const releaseId = route.query?._storyblok_release || 0
 
 const resolveRelations = []
@@ -52,7 +26,7 @@ const apiParams = {
 }
 
 const error404 = ref(false)
-console.log("SLUG", slug, route)
+
 
 let loadFallback = false
 try {
@@ -64,7 +38,7 @@ try {
 }
 if (loadFallback) {
     try {
-        console.log("ROUTE", route)
+
         if (slug === 'error-404') error404.value = true
         const { data } = await storyblokApi.get('cdn/stories/products-category/product-category', apiParams)
         story.value = data.story
@@ -86,7 +60,7 @@ try {
         })
     })
 } catch (error) {
-    console.log(error)
+    console.error(error)
 }
 
 
@@ -95,13 +69,26 @@ const config = useRuntimeConfig();
 
 let category = {};
 swell.init(config.swellStoreName, config.swellAccessToken);
-const { pending, data: ecommerceCategory } = await useLazyAsyncData('ecommerceCategory', function () {
+const { pending, data: ecommerceCategory } =  useLazyAsyncData('ecommerceCategory', function () {
     return swell.categories.get(slug)
 });
 
 watch(ecommerceCategory, (newEcommerceCategory) => {
-    console.log(newEcommerceCategory)
+    //console.log(newEcommerceCategory)
     category = newEcommerceCategory;
+})
+
+let products = {};
+
+
+const { pendingProducts, data: ecommerceProducts } =  useLazyAsyncData('ecommerceProducts', function () {
+    return swell.products.list(
+        { category: slug })
+});
+
+watch(ecommerceProducts, (newEcommerceProducts) => {
+    //console.log(newEcommerceProducts)
+    products = newEcommerceProducts;
 })
 
 
@@ -115,12 +102,24 @@ watch(ecommerceCategory, (newEcommerceCategory) => {
         <Headline v-if="story.content.headline">{{ story.content.headline }}</Headline>
         <StoryblokComponent v-if="story" :blok="story.content" :uuid="story.uuid" />
     </div>
-    <div v-if="pending" class="flex items-center justify-center">
+    <div v-if="pending" class="w-full flex items-center justify-center">
         <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
             role="status">
             <span
                 class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
         </div>
     </div>
-    <CategoryCardDetail v-else :key="category.id" :minimal=true :category="category" theme="light" />
+    <CategoryCardDetail v-else :key="category.id"  :category="category" theme="light" />
+
+    <div v-if="pendingProducts" class="w-full flex items-center justify-center">
+        <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+                class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+        </div>
+    </div>
+    <div  v-else class="items-center justify-center">
+        <ProductCard v-for="product in products.results" :key="product.id" :product="product"
+           />
+    </div>
 </template>
