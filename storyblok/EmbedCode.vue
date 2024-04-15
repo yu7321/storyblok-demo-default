df2d9db5420373cfec962f1fbef7f17f33ec6bfc
Roberto Butti <roberto.butti@gmail.com>
2024-04-15 17:00:24 +0200

    Create EmbedCode.vue

---
+++ storyblok/EmbedCode.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({ blok: Object })
+</script>
+
+<template>
+  <section
+    v-editable="blok"
+    class="page-section text-section relative z-50"
+    :class="[
+      'bg-' + blok.background_color,
+      {
+        'overlap-preceding-hero':
+          blok.overlap_preceding_hero && !blok.single_color_background,
+      },
+    ]"
+  >
+    <div class="container">
+      <td v-html="blok.snippet"></td>
+    </div>
+  </section>
+</template>
