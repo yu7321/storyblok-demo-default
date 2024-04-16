16e78317dd417e7c611024ffef28499c3dc5ca2c
Roberto Butti <roberto.butti@gmail.com>
2024-04-16 12:22:02 +0200

    Document list

---
+++ storyblok/DocumentItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({ doc: Object })
+</script>
+
+<template>
+  <li>
+    <div class="px-4 py-5 sm:px-6">
+      <div class="flex items-center justify-between">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">
+          {{ doc.name }}
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          {{ doc.description }}
+        </p>
+      </div>
+      <div class="mt-4 flex items-center justify-between">
+        <p class="text-sm font-medium text-gray-500">
+          Status:
+          <span class="text-green-600">Active</span>
+        </p>
+        <a
+          :href="doc.file.filename"
+          class="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          Download
+        </a>
+      </div>
+    </div>
+  </li>
+</template>
---
+++ storyblok/Documents.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({ blok: Object })
+</script>
+
+<template>
+  <section v-editable="blok" class="">
+    <div class="wd-full container text-left">
+      <Headline v-if="blok.headline" class="mb-4">{{ blok.headline }}</Headline>
+      <Lead v-if="blok.lead">
+        {{ blok.lead }}
+      </Lead>
+      <ul
+        class="mx-auto mt-16 max-w-sm overflow-hidden bg-white shadow sm:rounded-md"
+      >
+        <ComponentCheck
+          v-for="(doc, index) in blok.docs"
+          :key="doc._uid"
+          :index="index"
+          :doc="doc"
+          :blok="doc"
+        />
+      </ul>
+    </div>
+  </section>
+</template>
