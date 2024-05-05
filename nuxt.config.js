38153dcffd9fe99afde49b56e2955f0d4180005d
yu7321 <141029916+yu7321@users.noreply.github.com>
2024-05-05 13:37:24 +0200

    first commit
    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,8 +7,7 @@ export default defineNuxtConfig({
         If you would like to use this as a template for your project, simply provide the access token here.
         Also please remove line 14 (usePlugin) as well as the file plugins/storyblok.js.
         */
-        // accessToken: process.env.STORYBLOK_TOKEN,
-        usePlugin: false,
+        accessToken: 'MdwgPSjwSmQQcLgsiUFivwtt',
       },
     ],
     '@nuxtjs/tailwindcss',
@@ -20,8 +19,6 @@ export default defineNuxtConfig({
     public: {
       templateToken: process.env.STORYBLOK_TOKEN,
       customParent: process.env.STORYBLOK_CUSTOM_PARENT,
-      shopifyDomain: process.env.SHOPIFY_DOMAIN,
-      shopifyToken: process.env.SHOPIFY_TOKEN,
     },
   },
   vite: {
